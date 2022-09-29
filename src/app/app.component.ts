a16d0659937caf86c46e9e3751d51124148f8c41
John Mora <1987jhonjaime@gmail.com>
2022-09-28 20:56:04 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Empleado } from './empleado.Model';
 import { Component } from '@angular/core';
+import { ServicioEmpleadosService } from './servicio-empleados.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+
+  constructor(private miServicio:ServicioEmpleadosService){}
+
+  titulo = 'Listado de empleados';
+
+  empleados:Empleado[]=[
+
+    new Empleado('harold', 'reyes', 'joder', 500000),
+    new Empleado('kelly', 'sanchez', 'proferosa', 1300000),
+    new Empleado('aleida', 'prado', 'modista', 1000000),
+    new Empleado('john', 'mora', 'programador', 2000000)
+
+  ];
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.miServicio.muestraMensaje('nombre del empleado: ' + miEmpleado.nombre + '\napellido: ' + miEmpleado.apellido + '\ncargo: ' + miEmpleado.cargo + '\nsalario: ' + miEmpleado.salario);
+    this.empleados.push(miEmpleado);
+  }
+
+  cuadroNombre:string='';
+  cuadroApellido:string='';
+  cuadroCargo:string='';
+  cuadroSalario:number=0;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadoHijoCComponent } from './empleado-hijo-c/empleado-hijo-c.component';
+import { CaracteristicasEmpleadoComponent } from './caracteristicas-empleado/caracteristicas-empleado.component';
+import { ServicioEmpleadosService } from './servicio-empleados.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoHijoCComponent,
+    CaracteristicasEmpleadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ServicioEmpleadosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
