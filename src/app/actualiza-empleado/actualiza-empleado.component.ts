c4b2935ca0de05dd3add0f67947de4f8574fba81
John Mora <1987jhonjaime@gmail.com>
2022-09-30 15:28:45 -0500

    se creo por route actualiza y eliminar empleados

--- src/app/actualiza-empleado/actualiza-empleado.component.ts
+++ src/app/actualiza-empleado/actualiza-empleado.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Empleado } from '../empleado.Model';
 import { EmpleadosService } from '../empleados.service';
@@ -10,25 +11,50 @@ import { ServicioEmpleadosService } from '../servicio-empleados.service';
 })
 export class ActualizaEmpleadoComponent implements OnInit {
 
-  constructor(private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosService) { }
+  constructor(private router:Router, private route:ActivatedRoute, private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosService) { }
 
   ngOnInit(): void {
     this.empleados = this.empleadosService.empleados;
-  }
+    this.indice = this.route.snapshot.params['id'];
+    let empleado:Empleado=this.empleadosService.encontrarEmpleadoService(this.indice);
+    this.cuadroNombre = empleado.nombre;
+    this.cuadroApellido = empleado.apellido;
+    this.cuadroCargo = empleado.cargo;
+    this.cuadroSalario = empleado.salario;
 
-  titulo = 'Listado de empleados';
+    this.accion = this.route.snapshot.queryParams['accion'];
 
-  empleados:Empleado[]=[];
-
-  agregarEmpleado(){
-    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
-    this.miServicio.muestraMensaje('nombre del empleado: ' + miEmpleado.nombre + '\napellido: ' + miEmpleado.apellido + '\ncargo: ' + miEmpleado.cargo + '\nsalario: ' + miEmpleado.salario);
-    this.empleadosService.agregarEmpleadoServicio(miEmpleado)
   }
 
   cuadroNombre:string='';
   cuadroApellido:string='';
   cuadroCargo:string='';
   cuadroSalario:number=0;
+
+  titulo = 'Listado de empleados';
+
+  indice:number;
+
+  empleados:Empleado[]=[];
+
+  accion:string;
+  mensajeBoton:string = this.route.snapshot.queryParams['accion'];
+
+
+  actualizaElimina(){
+    if (this.accion == 'actualizar') {
+      let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+      this.miServicio.muestraMensaje('nombre del empleado: ' + miEmpleado.nombre + '\napellido: ' + miEmpleado.apellido + '\ncargo: ' + miEmpleado.cargo + '\nsalario: ' + miEmpleado.salario);
+      this.empleadosService.actualizarEmpleadoService(this.indice, miEmpleado);
+      this.router.navigate(['']);
+    }else{
+      this.empleadosService.eliminarEmpleadoService(this.indice);
+      this.router.navigate(['']);
+    }
+  }
+
+  volverHome(){
+    this.router.navigate(['']);
+  }
 
 }
--- src/app/empleados.service.ts
+++ src/app/empleados.service.ts
@@ -18,5 +18,22 @@ export class EmpleadosService {
     this.empleados.push(empleado);
   }
 
+  encontrarEmpleadoService(indice:number){
+    let empleado:Empleado = this.empleados[indice];
+    return empleado;
+  }
+
+  actualizarEmpleadoService(indice:number, empleado:Empleado){
+    let empleadoModificado = this.empleados[indice];
+    empleadoModificado.nombre = empleado.nombre;
+    empleadoModificado.apellido = empleado.apellido;
+    empleadoModificado.cargo = empleado.cargo;
+    empleadoModificado.salario = empleado.salario;
+  }
+
+  eliminarEmpleadoService(indice:number){
+    this.empleados.splice(indice, 1);
+  }
+
   constructor() { }
 }
